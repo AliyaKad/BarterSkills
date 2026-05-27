a3273113a01b76b033467ed45f1ed8c48c281d34
teruuj1 <shuvalova05sofya@mail.ru>
2026-05-27 20:59:24 +0300

    some details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>  {/* ✅ ОБЁРТКА */}
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
